b0af755859df2e7a6e8cafbab1dae458f4acb4aa
Yann-Baelden <yannougdar@yahoo.fr>
2025-03-02 18:47:49 +0100

    B bis: solution

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 from textnode import TextNode, TextType
 
-print("hello world")
 
 def main():
-    new_TextNode = TextNode("This is a text", TextType.BOLD_TEXT, "https://www.boot.dev")
-    print(new_TextNode)
+    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+    print(node)
+
 
 main()
