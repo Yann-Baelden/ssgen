7bb97045078becd295193256b28036baddec777d
Yann-Baelden <yannougdar@yahoo.fr>
2025-03-08 20:48:23 +0100

    Hbis: solutions

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType, text_node_to_html_node, split_nodes_delimiter
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -53,22 +53,6 @@ class TestTextNodeToHTMLNode(unittest.TestCase):
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "b")
         self.assertEqual(html_node.value, "This is bold")
-
-class TestSplitNodesDelimiter(unittest.TestCase):
-    def test_split_with_backtick(self):
-        node = TextNode("This is text with a `code block` word", TextType.TEXT)
-        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
-        self.assertEqual(new_nodes, [TextNode("This is text with a ", TextType.TEXT), TextNode("code block", TextType.CODE), TextNode(" word", TextType.TEXT)])
-
-    def test_split_with_asterisk(self):
-        node = TextNode("This is text with a *italic* word", TextType.TEXT)
-        new_nodes = split_nodes_delimiter([node], "*", TextType.ITALIC)
-        self.assertEqual(new_nodes, [TextNode("This is text with a ", TextType.TEXT), TextNode("italic", TextType.ITALIC), TextNode(" word", TextType.TEXT)])
-
-    def test_split_with_asterisks(self):
-        node = TextNode("This is text with a **bold** word", TextType.TEXT)
-        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
-        self.assertEqual(new_nodes, [TextNode("This is text with a ", TextType.TEXT), TextNode("bold", TextType.BOLD), TextNode(" word", TextType.TEXT)])
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -29,35 +29,16 @@ class TextNode:
 
 
 def text_node_to_html_node(text_node):
-    match (text_node.text_type):
-        case (TextType.TEXT):
-            return LeafNode(None, text_node.text)
-        case (TextType.BOLD):
-            return LeafNode("b", text_node.text)
-        case (TextType.ITALIC):
-            return LeafNode("i", text_node.text)
-        case (TextType.CODE):
-            return LeafNode("code", text_node.text)
-        case (TextType.LINK):
-            return LeafNode("a", text_node.text, {"href": text_node.url})
-        case (TextType.IMAGE):
-            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
-        case _:
-            raise ValueError(f"invalid text type: {text_node.text_type}")
-
-def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    result = []
-    for old_node in old_nodes:
-        if delimiter in old_node.text:
-            splited_node = old_node.text.split(delimiter)
-
-            for index, piece in enumerate(splited_node):
-                if index % 2 == 0:
-                    new_node = TextNode(piece, TextType.TEXT)
-                else:
-                    new_node = TextNode(piece, text_type)
-                result.append(new_node)
-        else:
-            result.append(old_node)
-    
-    return result
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode(None, text_node.text)
+    if text_node.text_type == TextType.BOLD:
+        return LeafNode("b", text_node.text)
+    if text_node.text_type == TextType.ITALIC:
+        return LeafNode("i", text_node.text)
+    if text_node.text_type == TextType.CODE:
+        return LeafNode("code", text_node.text)
+    if text_node.text_type == TextType.LINK:
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    if text_node.text_type == TextType.IMAGE:
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    raise ValueError(f"invalid text type: {text_node.text_type}")
