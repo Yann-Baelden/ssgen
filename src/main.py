a5ce63cc65c2aff0ba0beb619bc59bfdcbf19877
Yann-Baelden <yannougdar@yahoo.fr>
2025-03-16 01:22:55 +0100

    Pbis: update deployement

--- src/main.py
+++ src/main.py
@@ -13,6 +13,7 @@ template_path = "./template.html"
 
 
 def main():
+    print(f"valeur de sys.argv : {sys.argv}")
     if len(sys.argv) > 1:
         basepath = sys.argv[1]
     else:
@@ -28,7 +29,6 @@ def main():
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
 
-    print("Generating content...")
     generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 
