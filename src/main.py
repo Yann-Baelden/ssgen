2570433e2723e86b446f789e52cc26754c5a296e
Yann-Baelden <yannougdar@yahoo.fr>
2025-03-16 01:28:11 +0100

    Pter: solution

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
-import sys
 import os
 import shutil
+import sys
 
 from copystatic import copy_files_recursive
 from gencontent import generate_pages_recursive
@@ -10,17 +10,13 @@ dir_path_static = "./static"
 dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
+default_basepath = "/"
 
 
 def main():
-    print(f"valeur de sys.argv : {sys.argv}")
+    basepath = default_basepath
     if len(sys.argv) > 1:
         basepath = sys.argv[1]
-    else:
-        basepath = "/"
-
-    output_dir = "docs"
-    os.makedirs(output_dir, exist_ok=True)
 
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
@@ -29,6 +25,7 @@ def main():
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
 
+    print("Generating content...")
     generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 
